f86771750e4f54605ffa04c0659cf87963b0420c
alexa1999 <anel.veliz@ucsp.edu.pe>
2022-10-07 20:00:32 -0500

    Update rtt.cpp

--- lab-6/rtt/rtt/rtt.cpp
+++ lab-6/rtt/rtt/rtt.cpp
@@ -11,7 +11,7 @@ int main() {
 
 
     if (TestMode) {
-        cout << "SPlitTest:\n";
+        
         std::vector<HyperRect<DIMENSION>> myRectSet;
         std::vector<Entry<double, DIMENSION>> newroot;
 
